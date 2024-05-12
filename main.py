e2a38168919a4675ad633bf3dc18be5d321b1d9d
nipun thejan <76776959+nipun-thejan@users.noreply.github.com>
2024-05-13 00:03:43 +0530

    done

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-# import numpy as np
-# from verifyvoice import M
-
-
-# test_path = '/media/thejan/ubuntu_data/wav_test/'
-# filename = test_path + 'id10270/5r0dWxy17C8/00001.wav'
-
-# # model = get_model()
-# get_emb(filename)
-# # s = DataLoader.load_audio(sample1, 160)
-# # print(len(s))
-# # eb =get_emb(sample1, model)
-
-# # e1 = np.array([1, 2, 3, 4, 5])
-# # e2 = np.array([6, 7, 8, 9, 10])
-
-# # si = Similarity.cosine(e1, e2)
-# # print(si)
-
 from verifyvoice import ModelLoader
-
+from verifyvoice import Similarity
 
 model_path='/home/thejan/Downloads/model000000013.model'
 
 model = ModelLoader(model_path=model_path)
 
 spk_1_audio_1_path = '/media/thejan/ubuntu_data/wav_test/id10270/OmSWVqpb-N0/00001.wav'
+spk_1_audio_2_path = '/media/thejan/ubuntu_data/wav_test/id10270/5r0dWxy17C8/00001.wav'
+spk_2_audio_1_path = '/media/thejan/ubuntu_data/wav_test/id10282/37XQxZ5lBD8/00001.wav'
 
-emb = model.get_embedding(spk_1_audio_1_path)
-print(emb)
-print(emb.shape)
+emb1 = model.get_embedding(spk_1_audio_1_path)
+emb2 = model.get_embedding(spk_1_audio_2_path)
+emb3 = model.get_embedding(spk_2_audio_1_path)
+
+# print(emb)
+# print(emb.shape)
+print(f'Similarity of Same Speaker : {Similarity.cosine(emb1, emb2)}')
+print(f'Similarity of Different Speaker ; {Similarity.cosine(emb1, emb3)}')
